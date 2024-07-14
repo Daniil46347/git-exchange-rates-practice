b7d2890cec067018d3be446adf33b38d3eb6d609
Daniil46347 <dan.valentinovitch@yandex.ru>
2024-07-14 22:10:02 +0000

    Update Main.cpp

--- git-exchange-rates-practice/git-exchange-rates-practice/Main.cpp
+++ git-exchange-rates-practice/git-exchange-rates-practice/Main.cpp
@@ -6,6 +6,6 @@ int main()
 {
     cout << "Laboratory work #4. GIT\n";
     cout << "Variant #4. git-exchange-rates-practice\n";
-    cout << "Author: Yana Iluchenko\n";
+    cout << "Author: Valentinovitch Daniil\n";
     return 0;
 }
